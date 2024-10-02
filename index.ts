831a3a80df745709ea7b09e745de79e1064f95af
Alex Serra <bounteous17@protonmail.com>
2024-10-02 23:46:21 +0200

    nginx tls

--- index.ts
+++ index.ts
@@ -2,11 +2,12 @@ import { name as nginxDeployment } from "./deployment/nginx";
 import { name as traefikHelm } from "./helm/traefik";
 import { name as certManager } from "./helm/cert-manager";
 import * as traefikManifest from "./manifest/traefik";
+import * as certManagerManifest from "./manifest/cert-manager";
 import * as service from "./service";
 
 export default {
   deployments: { nginxDeployment },
   helm: { traefikHelm, certManager },
-  manifest: { traefikManifest },
+  manifest: { traefikManifest, certManagerManifest },
   service: { service },
 };
