625b4990a5ab6a2333e7a503f962708c7f6360fd
Alex Serra <bounteous17@protonmail.com>
2024-10-20 17:02:02 +0200

    chore: cert-manager helm-chart

--- helm-chart/cert-manager/index.ts
+++ helm-chart/cert-manager/index.ts
@@ -1,16 +1,35 @@
 import * as k8s from "@pulumi/kubernetes";
 
-new k8s.helm.v3.Release("cert-manager", {
+const release = new k8s.helm.v3.Release("cert-manager", {
+  name: "cert-manager",
   chart: "cert-manager",
   version: "1.15.3",
+  namespace: "cert-manager",
+  createNamespace: true,
   repositoryOpts: {
     repo: "https://charts.jetstack.io",
   },
   values: {
-    installCRDs: true
-  }
+    installCRDs: true,
+  },
 });
 
-new k8s.yaml.ConfigFile("cert-manager-issuer", {
-  file: "helm-chart/cert-manager/issuer.yaml",
-});
+new k8s.yaml.ConfigFile(
+  "cert-manager-production-issuer",
+  {
+    file: "helm-chart/cert-manager/production-issuer.yaml",
+  },
+  {
+    dependsOn: release,
+  }
+);
+
+new k8s.yaml.ConfigFile(
+  "cert-manager-staging-issuer",
+  {
+    file: "helm-chart/cert-manager/staging-issuer.yaml",
+  },
+  {
+    dependsOn: release,
+  }
+);
